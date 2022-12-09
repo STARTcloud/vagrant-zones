382038e7a5013364e1730495d6d6b4f12c084d76
Mark Gilbert <50398495+MarkProminic@users.noreply.github.com>
2022-12-09 16:06:58 -0600

    core: allow empty gateways

--- lib/vagrant-zones/driver.rb
+++ lib/vagrant-zones/driver.rb
@@ -1066,9 +1066,11 @@ module VagrantPlugins
         uii.info("  #{vnic_name}")
         netplan1 = %(network:\n  version: 2\n  ethernets:\n    #{vnic_name}:\n      match:\n        macaddress: #{mac}\n)
         netplan2 = %(      dhcp-identifier: mac\n      dhcp4: #{opts[:dhcp4]}\n      dhcp6: #{opts[:dhcp6]}\n)
-        netplan3 = %(      set-name: #{vnic_name}\n      addresses: [#{ip}/#{shrtsubnet}]\n      gateway4: #{defrouter}\n)
-        netplan4 = %(      nameservers:\n        addresses: [#{servers[0]['nameserver']} , #{servers[1]['nameserver']}] )
-        netplan = netplan1 + netplan2 + netplan3 + netplan4
+        netplan3 = %(      set-name: #{vnic_name}\n      addresses: [#{ip}/#{shrtsubnet}]\n)
+        netplan4 = %(      gateway4: #{defrouter}\n)
+        netplan5 = %(      nameservers:\n        addresses: [#{servers[0]['nameserver']} , #{servers[1]['nameserver']}] )
+        netplan = netplan1 + netplan2 + netplan3 + netplan5 if opts[:gateway].nil?
+        netplan = netplan1 + netplan2 + netplan3 + netplan4 + netplan5 unless opts[:gateway].nil?
         cmd = "echo '#{netplan}' > /etc/netplan/#{vnic_name}.yaml"
         infomessage = I18n.t('vagrant_zones.netplan_applied_static') + "/etc/netplan/#{vnic_name}.yaml"
         uii.info(infomessage) if zlogin(uii, cmd)
