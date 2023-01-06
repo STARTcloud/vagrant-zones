718c2096d2ac37a44a92234901ca7a73664cd088
Mark Gilbert <50398495+MarkProminic@users.noreply.github.com>
2023-01-06 05:09:46 -0600

    fix: ruby lint

--- lib/vagrant-zones/errors.rb
+++ lib/vagrant-zones/errors.rb
@@ -5,54 +5,67 @@ require 'vagrant'
 module VagrantPlugins
   module ProviderZone
     module Errors
+      # Namespace for Vagrant Zones Errors
       class VagrantZonesError < Vagrant::Errors::VagrantError
         error_namespace('vagrant_zones.errors')
       end
 
+      # System Check Results
       class SystemVersionIsTooLow < VagrantZonesError
         error_key(:system_version_too_low)
       end
 
+      # Compatability Check Tool 
       class MissingCompatCheckTool < VagrantZonesError
         error_key(:missing_compatability_check_tool)
       end
 
+      # Missing Bhyve
       class MissingBhyve < VagrantZonesError
         error_key(:missing_bhyve)
       end
 
+      # HasNoRootPrivilege
       class HasNoRootPrivilege < VagrantZonesError
         error_key(:has_no_root_privilege)
       end
 
+      # ExecuteError
       class ExecuteError < VagrantZonesError
         error_key(:execute_error)
       end
 
+      # TimeoutError
       class TimeoutError < VagrantZonesError
         error_key(:timeout_error)
       end
 
+      # VirtualBoxRunningConflictDetected
       class VirtualBoxRunningConflictDetected < VagrantZonesError
         error_key(:virtual_box_running_conflict_detected)
       end
 
+      # NotYetImplemented
       class NotYetImplemented < VagrantZonesError
         error_key(:not_yet_implemented)
       end
 
+      # TimeoutHalt
       class TimeoutHalt < VagrantZonesError
         error_key(:halt_timeout)
       end
 
+      # InvalidbhyveBrand
       class InvalidbhyveBrand < VagrantZonesError
         error_key(:invalidbhyve_brand)
       end
 
+      # InvalidLXBrand
       class InvalidLXBrand < VagrantZonesError
         error_key(:invalidLX_brand)
       end
 
+      # ConsoleFailed
       class ConsoleFailed < VagrantZonesError
         error_key(:console_failed_exit)
       end
