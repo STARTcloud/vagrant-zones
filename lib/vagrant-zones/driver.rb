c58a26c9d6d31a0ffc756229b2a2969decea71dd
Mark Gilbert <50398495+MarkProminic@users.noreply.github.com>
2023-02-05 01:22:28 -0600

    chore: escaping

--- lib/vagrant-zones/driver.rb
+++ lib/vagrant-zones/driver.rb
@@ -274,7 +274,7 @@ module VagrantPlugins
               Timeout.timeout(config.clean_shutdown_time) do
                 loop do                  
                   zlogin_read.expect(/\r\n/) { |line| responses.push line }
-                  p (responses[-1]) if config.debug_boot and !responses[-1].empty?
+                  p (responses[-1]) if config.debug_boot and !responses[-1].nil?
                   if responses[-1].to_s.match(/((?:[0-9]{1,3}\.){3}[0-9]{1,3})/)
                     p (responses[-1]) if config.debug_boot
                     ip = responses[-1].to_s.match(/((?:[0-9]{1,3}\.){3}[0-9]{1,3})/).captures
