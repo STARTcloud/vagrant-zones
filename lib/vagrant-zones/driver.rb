08dedab70d7b38c678174f4e8661681b2a79c7bf
Mark Gilbert <50398495+MarkProminic@users.noreply.github.com>
2023-02-05 00:24:28 -0600

    chore: change

--- lib/vagrant-zones/driver.rb
+++ lib/vagrant-zones/driver.rb
@@ -269,10 +269,10 @@ module VagrantPlugins
             command = %(ip -4 addr show dev #{vnic_name} | head -n -1 | tail -1 | awk '{ print $2 }' | cut -f1 -d"/")
             PTY.spawn("pfexec zlogin -C #{name}") do |zlogin_read, zlogin_write, pid|
               responses = []
-              zlogin_read.expect(/\n/) { zlogin_write.puts(command) } if i == 0
-              i += 1 
               Timeout.timeout(config.clean_shutdown_time) do
                 loop do
+                  zlogin_read.expect(/\n/) { zlogin_write.puts(command) } if i == 0
+                  i += 1 
                   zlogin_read.expect(/\r\n/) { |line| responses.push line }
                   p (responses[-1]) if config.debug_boot
 
