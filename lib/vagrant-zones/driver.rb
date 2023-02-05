724b523dad3ac1f40edf892567f2413132e8a925
Mark Gilbert <50398495+MarkProminic@users.noreply.github.com>
2023-02-04 23:15:19 -0600

    chore: save

--- lib/vagrant-zones/driver.rb
+++ lib/vagrant-zones/driver.rb
@@ -272,7 +272,7 @@ module VagrantPlugins
               Timeout.timeout(config.clean_shutdown_time) do
                 loop do
                   zlogin_read.expect(/\r\n/) { |line| responses.push line }
-                  printf(responses[-1])  if config.debug_boot
+                  p (responses[-1])  if config.debug_boot
                   if responses[-1].to_s.match(/(?:[0-9]{1,3}\.){3}[0-9]{1,3}/)
                     ip = responses[-1][0].rstrip.lstrip
                     return nil if ip.empty?
