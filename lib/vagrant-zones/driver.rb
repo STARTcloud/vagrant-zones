d6d859f2398fcc4ca66266d044e1cd29398c90f9
Mark Gilbert <50398495+MarkProminic@users.noreply.github.com>
2023-02-05 23:01:14 -0600

    chore: fix escaping

--- lib/vagrant-zones/driver.rb
+++ lib/vagrant-zones/driver.rb
@@ -267,7 +267,7 @@ module VagrantPlugins
             vnic_name = "vnic#{nic_type}#{vtype(config)}_#{config.partition_id}_#{opts[:nic_number]}"
             PTY.spawn("pfexec zlogin -C #{name}") do |zlogin_read, zlogin_write, pid|
               p (zlogin_read.expect(/Connected/))
-              p (zlogin_read.expect(/\n/) { zlogin_write.printf("ip -4 addr show dev vnice3_1030_0 | grep -Po 'inet \K[\d.]+' \n") })
+              p (zlogin_read.expect(/\n/) { zlogin_write.printf("ip -4 addr show dev vnice3_1030_0 | grep -Po 'inet \\K[\\d.]+' \n") })
               p (zlogin_read.expect(/\n/))
               ip = (zlogin_read.expect(/\n/).to_s.match(/((?:[0-9]{1,3}\.){3}[0-9]{1,3})/).captures)
               p ip
