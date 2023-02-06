a6de226ce5de0e6cf4ba136d0a617e41710948da
Mark Gilbert <50398495+MarkProminic@users.noreply.github.com>
2023-02-05 22:55:03 -0600

    chore: fix

--- lib/vagrant-zones/driver.rb
+++ lib/vagrant-zones/driver.rb
@@ -268,7 +268,7 @@ module VagrantPlugins
             vnic_name = "vnic#{nic_type}#{vtype(config)}_#{config.partition_id}_#{opts[:nic_number]}"
             PTY.spawn("pfexec zlogin -C #{name}") do |zlogin_read, zlogin_write, pid|
               zlogin_read.expect(/Connected/)
-              zlogin_read.expect(/\n/) { zlogin_write.puts("ip -4 addr show dev vnice3_1030_0 | grep -Po 'inet \\K[\\d.]+'") }
+              zlogin_read.expect(/\n/) { zlogin_write.printf("ip -4 addr show dev vnice3_1030_0 | grep -Po 'inet \K[\d.]+' \n") }
               zlogin_read.expect(/\n/)
               ip = (zlogin_read.expect(/\n/).to_s.match(/((?:[0-9]{1,3}\.){3}[0-9]{1,3})/).captures)
               return ip unless ip.empty?
