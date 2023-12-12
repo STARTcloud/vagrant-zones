c4619d8efab3e85c10bba2caaa26e20620bde788
Mark Gilbert <50398495+MarkProminic@users.noreply.github.com>
2023-12-11 23:59:09 -0600

    fix: add specific routing to netplan configs

--- lib/vagrant-zones/driver.rb
+++ lib/vagrant-zones/driver.rb
@@ -526,7 +526,7 @@ module VagrantPlugins
         uii.info("  #{vnic_name}")
         netplan1 = %(network:\n  version: 2\n  ethernets:\n    #{vnic_name}:\n      match:\n        macaddress: #{mac}\n)
         netplan2 = %(      dhcp-identifier: mac\n      dhcp4: #{opts[:dhcp4]}\n      dhcp6: #{opts[:dhcp6]}\n)
-        netplan3 = %(      set-name: #{vnic_name}\n      addresses: [#{ip}/#{shrtsubnet}]\n      routes:\n        - to: default\n          via: #{defrouter}\n)
+        netplan3 = %(      set-name: #{vnic_name}\n      addresses: [#{ip}/#{shrtsubnet}]\n      routes:\n        - to: #{opts[:route]}\n          via: #{defrouter}\n)
         netplan4 = %(      nameservers:\n        addresses: [#{servers}]) unless opts[:dns].nil?
         netplan = netplan1 + netplan2 + netplan3 + netplan4
         cmd = "echo -e '#{netplan}' | sudo tee /etc/netplan/#{vnic_name}.yaml && chmod 400 /etc/netplan/#{vnic_name}.yaml"
@@ -1143,7 +1143,7 @@ module VagrantPlugins
         netplan2 = %(      dhcp-identifier: mac\n      dhcp4: #{opts[:dhcp4]}\n      dhcp6: #{opts[:dhcp6]}\n) if opts[:dhcp4]
         netplan3 = %(      set-name: #{vnic_name}\n      addresses: [#{ip}/#{shrtsubnet}]\n)
         netplan3 = %(      set-name: #{vnic_name}\n) if opts[:dhcp4]
-        netplan4 = %(      routes:\n        - to: default\n          via: #{defrouter}\n)
+        netplan4 = %(      routes:\n        - to: #{opts[:route]}\n          via: #{defrouter}\n)
         netplan5 = %(      nameservers:\n        addresses: [#{servers}]) unless opts[:dns].nil?
         netplan = netplan1 + netplan2 + netplan3 + netplan5 if opts[:gateway].nil?
         netplan = netplan1 + netplan2 + netplan3 + netplan4 + netplan5 unless opts[:gateway].nil?
