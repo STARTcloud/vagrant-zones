5bf2ceaf3adaf42553afe7a38ab9c2a665d7ff21
Mark Gilbert <50398495+MarkProminic@users.noreply.github.com>
2023-12-12 01:01:24 -0600

    fix: enable parallel builds #dangerwillrobinson

--- lib/vagrant-zones/plugin.rb
+++ lib/vagrant-zones/plugin.rb
@@ -22,7 +22,7 @@ module VagrantPlugins
       end
       ## Experimental Parallel Execucution
       ## provider(:zone, parallel: true) do
-      provider(:zone) do
+      provider(:zone, parallel: true) do
         require_relative 'provider'
         Provider
       end
