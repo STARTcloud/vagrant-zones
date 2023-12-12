25e95432a904aee2c73e6d0b67b00f61c40ce576
Mark Gilbert <50398495+MarkProminic@users.noreply.github.com>
2023-12-11 21:09:44 -0600

    fix: rubocop caught some junk

--- lib/vagrant-zones/driver.rb
+++ lib/vagrant-zones/driver.rb
@@ -526,7 +526,6 @@ module VagrantPlugins
         uii.info("  #{vnic_name}")
         netplan1 = %(network:\n  version: 2\n  ethernets:\n    #{vnic_name}:\n      match:\n        macaddress: #{mac}\n)
         netplan2 = %(      dhcp-identifier: mac\n      dhcp4: #{opts[:dhcp4]}\n      dhcp6: #{opts[:dhcp6]}\n)
-        #netplan3 = %(      set-name: #{vnic_name}\n      addresses: [#{ip}/#{shrtsubnet}]\n      gateway4: #{defrouter}\n)
         netplan3 = %(      set-name: #{vnic_name}\n      addresses: [#{ip}/#{shrtsubnet}]\n      routes:\n        - to: default\n          via: #{defrouter}\n)
         netplan4 = %(      nameservers:\n        addresses: [#{servers}] )  unless opts[:dns].nil?
         netplan = netplan1 + netplan2 + netplan3 + netplan4
@@ -1144,7 +1143,6 @@ module VagrantPlugins
         netplan2 = %(      dhcp-identifier: mac\n      dhcp4: #{opts[:dhcp4]}\n      dhcp6: #{opts[:dhcp6]}\n) if opts[:dhcp4]
         netplan3 = %(      set-name: #{vnic_name}\n      addresses: [#{ip}/#{shrtsubnet}]\n)
         netplan3 = %(      set-name: #{vnic_name}\n) if opts[:dhcp4]
-        #netplan4 = %(      gateway4: #{defrouter}\n)
         netplan4 = %(      routes:\n        - to: default\n          via: #{defrouter}\n)
         netplan5 = %(      nameservers:\n        addresses: [#{servers}] ) unless opts[:dns].nil?
         netplan = netplan1 + netplan2 + netplan3 + netplan5 if opts[:gateway].nil?
