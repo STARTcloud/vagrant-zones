3559ebf193bd27315e6b0cf8b03bb5f3ed54d00d
Makr91 <mark.gilbert@m4kr.net>
2026-04-02 02:58:08 -0500

    fix: fixing the AI lies

--- lib/vagrant-zones/driver.rb
+++ lib/vagrant-zones/driver.rb
@@ -503,7 +503,8 @@ module VagrantPlugins
       def host_vnic_name(opts)
         config = @machine.provider_config
         if opts[:etherstub]
-          result = execute(false, "#{@pfexec} dladm show-vnic -o LINK,OVER | grep #{opts[:etherstub]} | awk '{ print $1 }' | head -n 1 || true")
+          cmd = "#{@pfexec} dladm show-vnic -o LINK,OVER,ZONE -p | grep #{opts[:etherstub]} | grep ':\\-\\-$' | awk -F: '{ print $1 }' | head -n 1"
+          result = execute(false, "#{cmd} || true")
           return result.strip unless result.strip.empty?
         end
         "h_vnic_#{config.partition_id}_#{opts[:nic_number]}"
